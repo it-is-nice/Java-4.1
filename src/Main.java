fec891a90693a2e4a43e656c38206b6fa89c2d8b
Max Vertlib <maximvert@gmail.com>
2021-03-12 09:51:59 +0300

    Add new sout line

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         // Для подсчитывания количества миль за билет, подставьте в 4 строку, значения price стоимтость билета
-        int price = 1;
+        int price = 999;
         int miles = price / 20;
-        System.out.println(miles);
+        System.out.println(miles + " miles for " + price + " dollars");
     }
 }
