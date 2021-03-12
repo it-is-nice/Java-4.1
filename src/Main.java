f6b8962b8745134691fadf956b5f8d9ff665010a
Max Vertlib <maximvert@gmail.com>
2021-03-12 09:26:38 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // Для подсчитывания количества миль за билет, подставьте в 4 строку, значения price стоимтость билета
+        int price = 1;
+        int miles = price / 20;
+        System.out.println(miles);
+    }
+}
