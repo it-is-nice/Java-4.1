a9c7cb1d1f5d1c0fdb947adedf770903d3b8676b
Max Vertlib <maximvert@gmail.com>
2021-03-12 10:08:53 +0300

    Add rubles instead dollars

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
         // Для подсчитывания количества миль за билет, подставьте в 4 строку, значения price стоимтость билета
         int price = 999;
         int miles = price / 20;
-        System.out.println(miles + " miles for " + price + " dollars");
+        System.out.println(miles + " miles for " + price + " rubles");
     }
 }
