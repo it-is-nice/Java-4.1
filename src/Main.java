55942ce3bc82658b4276339e4b25db655f244a5f
Max Vertlib <maximvert@gmail.com>
2021-03-15 15:16:19 +0300

    Add CamalCase

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         // Для подсчитывания количества миль за билет, подставьте в 4 строку, значения price стоимтость билета
-        int price = 999;
-        int miles = price / 20;
-        System.out.println(miles + " miles for " + price + " rubles");
+        int airPrice = 999;
+        int milesBonus = airPrice / 20;
+        System.out.println(milesBonus + " miles for " + airPrice + " rubles");
     }
 }
