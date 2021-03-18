675c29a0fda84e93fd0e0b28693f7fd2399cd0ed
Max Vertlib <maximvert@gmail.com>
2021-03-18 11:18:51 +0300

    Add new code

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        // Для подсчитывания количества миль за билет, подставьте в 4 строку, значения price стоимтость билета
-        int airPrice = 999;
-        int milesBonus = airPrice / 20;
-        System.out.println(milesBonus + " miles for " + airPrice + " rubles");
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
